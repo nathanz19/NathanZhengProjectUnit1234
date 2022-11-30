bdb61b729995b8450a7f0ab5ba671f8071009d5b
Nathan Zheng <nathanz@nycstudents.net>
2022-11-29 19:58:02 -0500

    update

--- src/Contact.java
+++ src/Contact.java
@@ -15,13 +15,13 @@ public class Contact {
     }
     public Contact(String name) {
         this.name = name;
-        number = "(718) 804-6400";
+        number = "718-804-6400";
         address = "29 Fort Greene Pl, Brooklyn, NY 11217";
         birthday = "1/1/2006";
     }
     public Contact() {
         name = "John Doe";
-        number = "(718) 804-6400";
+        number = "718-804-6400";
         address = "29 Fort Greene Pl, Brooklyn, NY 11217";
         birthday = "1/1/2006";
     }
@@ -70,40 +70,49 @@ public class Contact {
         int tdyDay = Integer.parseInt(todaysDate.substring(todaysDate.indexOf("/")+1));
 
         String bday = birthday.substring(0, birthday.length()-5);
-        int bdayMonth = Integer.parseInt(bday.substring(0,bday.indexOf("/"))) +12;
+        int bdayMonth = Integer.parseInt(bday.substring(0,bday.indexOf("/")));
         int bdayDay = Integer.parseInt(bday.substring(bday.indexOf("/")+1));
 
         int days = 0;
+        int difference = 0;
+        int monthOfTdy = tdyMonth;
+        int dayOfTdy = tdyDay;
+        int dayOfBday = bdayDay;
 
-//        if (bdayDay < tdyDay) {
-//            bdayMonth --;
-//        }
+        if (tdyMonth > bdayMonth) {
+            difference = (bdayMonth + 12) - tdyMonth;
+        } else {
+            difference = bdayMonth - tdyMonth;
+        }
 
-        if ()
-//            if (tdyMonth > 12) {
-//                if ((tdyMonth == 13) || ((tdyMonth <= 19) && (tdyMonth % 2 == 1)) || ((tdyMonth >= 20) && (tdyMonth % 2 == 1))) {
-//                    days += 31;
-//                } else if (((tdyMonth <= 19) && (tdyMonth % 2 == 0) && (tdyMonth != 14)) || ((tdyMonth >= 20) && (tdyMonth % 2 == 0))) {
-//                    days += 30;
-//                } else {
-//                    days += 28;
-//                }
-//            } else {
-//                if ((tdyMonth == 1) || ((tdyMonth <= 7) && (tdyMonth % 2 == 1)) || ((tdyMonth >= 8) && (tdyMonth % 2 == 1))) {
-//                    days += 31;
-//                } else if (((tdyMonth <= 7) && (tdyMonth % 2 == 0) && (tdyMonth != 2)) || ((tdyMonth >= 8) && (tdyMonth % 2 == 0))) {
-//                    days += 30;
-//                } else {
-//                    days += 28;
-//                }
-//            }
-//            tdyMonth++;
-//        }
+        if (difference == 0 && dayOfBday > dayOfTdy) {
+            days += dayOfBday - dayOfTdy;
+        }
+        if (difference == 0 && dayOfBday < dayOfTdy) {
+            days += 365 - (dayOfTdy - dayOfBday);
+        }
+
+        for (int i = 0; i < difference; i++) {
+            if ((monthOfTdy == 1) || ((monthOfTdy <= 7) && (monthOfTdy % 2 == 1)) || ((monthOfTdy >= 8) && (monthOfTdy % 2 == 1))) {
+                days += 31;
+            } else if (((monthOfTdy <= 7) && (monthOfTdy % 2 == 0) && (monthOfTdy != 2)) || ((monthOfTdy >= 8) && (monthOfTdy % 2 == 0))) {
+                days += 30;
+            } else {
+                days += 28;
+            }
+            monthOfTdy++;
+        }
+        if (bdayDay != tdyDay) {
+            days = days - tdyDay + bdayDay;
+        }
+
         if (days == 0) {
             System.out.println("Your birthday is today! Happy Birthday!");
         } else {
             System.out.println("You have " + days + " days until your birthday!");
         }
-
     }
+//    public String toString() {
+//        String stringPaste =
+//    }
 }
